78fe09e347b9134c6fb4bc81bbde8a6aca3fc0ea
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-05-12 17:45:19 -0600

    Up

--- components/ui/ButtonPrimary.jsx
+++ components/ui/ButtonPrimary.jsx
@@ -6,11 +6,13 @@ import Link from "next/link";
 const ButtonPrimary = ({contentButton}) => (
   <div className="w-full p-4 flex justify-center items-center">
     <section className="text-gray-600 body-font flex justify-center items-center">
-      <p className="bg-red-600 hover:bg-primary-dark text-white font-bold py-6 px-12 rounded-full">
+     
         <Link href="/pagar">
+        <p className="bg-red-600 hover:bg-primary-dark text-white font-bold py-6 px-12 rounded-full">
          {contentButton}
+         </p>
         </Link>
-      </p>
+     
     </section>
   </div>
 );
